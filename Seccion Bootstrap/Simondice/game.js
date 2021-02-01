3da6c8321e0faf451147e32601d3a5d437b70ff0
AlbertCos <cosmenalberto@gmail.com>
2021-02-01 11:31:09 +0100

    Añadida funcion timout con JQuery para cambiar clase .pressed en proyecto SimonDice

--- Seccion Bootstrap/Simondice/game.js
+++ Seccion Bootstrap/Simondice/game.js
@@ -1,38 +1,42 @@
 prompt("Hello");
 
-var buttonColours=["red", "blue", "green","yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
-var userClickedPattern=[];
+var userClickedPattern = [];
 
-$(".btn").click(function(){
-var userChosenColour =  $(this).attr("id");
-playsound(userChosenColour);
-userClickedPattern.push(userChosenColour);
-console.log(userClickedPattern);
-nextSequence();
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  playsound(userChosenColour);
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+  animatePress(userChosenColour);
+  nextSequence();
 });
 
-function playsound(name){
+function animatePress(currentColour) {
+  setTimeout(function() {
+    $("#" + currentColour).addClass("pressed");
+  }, 100);
+
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
+  },200);
+
+};
+
+function playsound(name) {
 
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 };
 
-function nextSequence(){
+function nextSequence() {
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-
-  $(document). ready(() => {
-    setInterval(() => {
-    $("#" + randomChosenColour). fadeIn();
-    $("#" + randomChosenColour). fadeOut();
-    }, 100);
-  });
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
   playsound(randomChosenColour);
 };
-
-nextSequence();
